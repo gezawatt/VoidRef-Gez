889388f8d78a1f9c88a480fe1279e908a8c8ca24
gezawatt <gordeev-pure@yandex.ru>
2021-06-01 08:04:12 +0300

    Initial commit

---
+++ VoidRef-Gez/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace VoidRef_Gez
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.enlargeCB = new System.Windows.Forms.CheckBox();
+            this.reduceCB = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.deleteCB = new System.Windows.Forms.CheckBox();
+            this.normalizeCB = new System.Windows.Forms.CheckBox();
+            this.normalizeTB = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(271, 130);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pb_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pb_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pb_MouseUp);
+            // 
+            // enlargeCB
+            // 
+            this.enlargeCB.AutoSize = true;
+            this.enlargeCB.Location = new System.Drawing.Point(12, 12);
+            this.enlargeCB.Name = "enlargeCB";
+            this.enlargeCB.Size = new System.Drawing.Size(100, 19);
+            this.enlargeCB.TabIndex = 1;
+            this.enlargeCB.Text = "Увеличить на";
+            this.enlargeCB.UseVisualStyleBackColor = true;
+            this.enlargeCB.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // reduceCB
+            // 
+            this.reduceCB.AutoSize = true;
+            this.reduceCB.Location = new System.Drawing.Point(12, 37);
+            this.reduceCB.Name = "reduceCB";
+            this.reduceCB.Size = new System.Drawing.Size(106, 19);
+            this.reduceCB.TabIndex = 2;
+            this.reduceCB.Text = "Уменьшить на";
+            this.reduceCB.UseVisualStyleBackColor = true;
+            this.reduceCB.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(59, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1.1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(59, 23);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "1.1";
+            // 
+            // deleteCB
+            // 
+            this.deleteCB.AutoSize = true;
+            this.deleteCB.Location = new System.Drawing.Point(12, 62);
+            this.deleteCB.Name = "deleteCB";
+            this.deleteCB.Size = new System.Drawing.Size(70, 19);
+            this.deleteCB.TabIndex = 5;
+            this.deleteCB.Text = "Удалить";
+            this.deleteCB.UseVisualStyleBackColor = true;
+            this.deleteCB.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // normalizeCB
+            // 
+            this.normalizeCB.AutoSize = true;
+            this.normalizeCB.Checked = true;
+            this.normalizeCB.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.normalizeCB.Location = new System.Drawing.Point(184, 10);
+            this.normalizeCB.Name = "normalizeCB";
+            this.normalizeCB.Size = new System.Drawing.Size(116, 19);
+            this.normalizeCB.TabIndex = 7;
+            this.normalizeCB.Text = "Нормализовать:";
+            this.normalizeCB.UseVisualStyleBackColor = true;
+            // 
+            // normalizeTB
+            // 
+            this.normalizeTB.Location = new System.Drawing.Point(300, 8);
+            this.normalizeTB.Name = "normalizeTB";
+            this.normalizeTB.Size = new System.Drawing.Size(39, 23);
+            this.normalizeTB.TabIndex = 8;
+            this.normalizeTB.Text = "200";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 450);
+            this.Controls.Add(this.normalizeTB);
+            this.Controls.Add(this.normalizeCB);
+            this.Controls.Add(this.deleteCB);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.reduceCB);
+            this.Controls.Add(this.enlargeCB);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Удалить";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox enlargeCB;
+        private System.Windows.Forms.CheckBox reduceCB;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox deleteCB;
+        private System.Windows.Forms.CheckBox normalizeCB;
+        private System.Windows.Forms.TextBox normalizeTB;
+    }
+}
+
---
+++ VoidRef-Gez/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace VoidRef_Gez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+        }
+
+        bool isPressed = false;        
+
+
+        private void moveImageUni(PictureBox tempPB)
+        {
+            Point tempPoint = PointToClient(Cursor.Position);
+            tempPoint.X -= tempPB.Width / 2;
+            tempPoint.Y -= tempPB.Height / 2;
+            tempPB.Location = tempPoint;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox clickedPB = (PictureBox)sender;
+            moveImageUni(clickedPB);
+        }
+
+
+        private void pb_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPressed = true;
+
+            if (deleteCB.Checked == true)
+            {
+                PictureBox clickedPB = (PictureBox)sender;
+                this.Controls.Remove(clickedPB);
+            }
+
+            if (enlargeCB.Checked == true)
+            {
+                PictureBox clickedPB = (PictureBox)sender;
+                double enlargeVal = Convert.ToDouble(textBox1.Text);
+                clickedPB.Height = Convert.ToInt32(clickedPB.Height * enlargeVal);
+                clickedPB.Width = Convert.ToInt32(clickedPB.Width * enlargeVal);
+            }
+
+            if (reduceCB.Checked == true)
+            {
+                PictureBox clickedPB = (PictureBox)sender;
+                double reduceVal = Convert.ToDouble(textBox2.Text);
+                clickedPB.Height = Convert.ToInt32(clickedPB.Height / reduceVal);
+                clickedPB.Width = Convert.ToInt32(clickedPB.Width / reduceVal);
+            }
+
+        }
+
+
+        private void pb_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed == true)
+            {
+                PictureBox clickedPB = (PictureBox)sender;
+                moveImageUni(clickedPB);
+            }
+        }
+
+
+        private void pb_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if (data != null)
+            {
+                var fileNames = data as string[];
+                if (fileNames.Length > 0)
+                {
+                    int tempIndex = 0;                        // HACKY, NEEDS FIXING !!!!!
+                    foreach (string element in fileNames)
+                    {
+                        //int index = fileNames.
+
+                        {
+                            var picture = new PictureBox
+                            {
+                                Name = "pictureBox",
+                                Location = PointToClient(Cursor.Position),
+                                Image = Image.FromFile(fileNames[tempIndex]),     // << fix
+                                Size = Image.FromFile(fileNames[tempIndex]).Size, // << fix
+                                SizeMode = PictureBoxSizeMode.Zoom,
+                            };
+
+                            if (normalizeCB.Checked == true)
+                            {
+                                if (picture.Height > Convert.ToInt32(normalizeTB.Text) )
+                                {
+                                    double diffCoeff = (Convert.ToDouble(normalizeTB.Text) / picture.Height);
+                                    picture.Height = Convert.ToInt32(picture.Height * diffCoeff);
+                                    picture.Width = Convert.ToInt32(picture.Width * diffCoeff);
+                                }
+                            }
+
+                            this.Controls.Add(picture);                            
+
+                            picture.MouseDown += new MouseEventHandler(pb_MouseDown);
+                            picture.MouseUp += new MouseEventHandler(pb_MouseUp);
+                            picture.MouseMove += new MouseEventHandler(pb_MouseMove);                            
+                            tempIndex++;                                          // << fix
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            reduceCB.Enabled = !reduceCB.Enabled;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            enlargeCB.Enabled = !enlargeCB.Enabled;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
